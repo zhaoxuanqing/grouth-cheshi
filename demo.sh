9373aa31754d91f515a608a014dfb8587a12faf4
zxq01158352@antgroup.com <zxq01158352@antgroup.com>
2023-07-10 14:47:58 +0800

    最新

--- demo.sh
+++ demo.sh
@@ -1,8 +1,10 @@
-output=$(git pull)
-
 echo "===== Git pull..."
-if [[ $output == *"Already up to date."* ]]; then
-  echo "没有文件更改。"
+if [[ $(git pull) != *"Already up to date."* ]]; then
+  if [[ $(git status --porcelain) ]]; then
+    echo "文件已更改。"
+  else
+    echo "没有文件更改。"
+  fi
 else
-  echo "文件已更改。"
+  echo "没有文件更改。"
 fi
