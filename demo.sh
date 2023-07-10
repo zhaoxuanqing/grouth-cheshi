68fa22f8426088df0edc69f78215823c486d19fd
zxq01158352@antgroup.com <zxq01158352@antgroup.com>
2023-07-10 14:39:20 +0800

    cs

--- demo.sh
+++ demo.sh
@@ -1,17 +1,8 @@
-input="/grouth-cheshi/.git/index"
 output=$(git pull)
 
-cd "$input"
 echo "===== Git pull..."
 if [[ $output == *"Already up to date."* ]]; then
   echo "没有文件更改。"
 else
   echo "文件已更改。"
 fi
-
-
-
-# 执行git pull命令，并将输出保存到变量中
-
-
-# 判断输出中是否包含 "Already up to date." 的消息
